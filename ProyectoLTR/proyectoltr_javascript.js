60c26939e10dc7fdc0f4027eb894c8eea996c66e
aareupGH <aaronperezmail@gmail.com>
2020-06-25 10:42:15 +0200

    format code

--- ProyectoLTR/proyectoltr_javascript.js
+++ ProyectoLTR/proyectoltr_javascript.js
@@ -1,33 +1,58 @@
 $(document).ready(function () {
-  $('#divRss').FeedEk({
-FeedUrl: 'https://www.loteriasyapuestas.es/es/la%2Dprimitiva/resultados/.formatoRSS', MaxCount: 2
-});
+  $("#divRss").FeedEk({
+    FeedUrl:
+      "https://www.loteriasyapuestas.es/es/la%2Dprimitiva/resultados/.formatoRSS",
+    MaxCount: 2,
+  });
 
-$('#divRss2').FeedEk({
-FeedUrl: 'https://jquery-plugins.net/rss', MaxCount: 3, ShowDesc: true, ShowPubDate: false, DescCharacterLimit: 100
-});
-$('#divRss3').FeedEk({
-FeedUrl: 'https://jquery-plugins.net/rss', MaxCount: 4, ShowDesc: false
-});
+  $("#divRss2").FeedEk({
+    FeedUrl: "https://jquery-plugins.net/rss",
+    MaxCount: 3,
+    ShowDesc: true,
+    ShowPubDate: false,
+    DescCharacterLimit: 100,
+  });
+  $("#divRss3").FeedEk({
+    FeedUrl: "https://jquery-plugins.net/rss",
+    MaxCount: 4,
+    ShowDesc: false,
+  });
 
-$('#divRss4').FeedEk({
-FeedUrl: 'https://jquery-plugins.net/rss', MaxCount: 2, DateFormat: 'd', DateFormatLang: 'en'
-});
+  $("#divRss4").FeedEk({
+    FeedUrl: "https://jquery-plugins.net/rss",
+    MaxCount: 2,
+    DateFormat: "d",
+    DateFormatLang: "en",
+  });
 
-$('#divRss5').FeedEk({
-FeedUrl: 'https://jquery-plugins.net/rss', MaxCount: 2, DateFormat: 'D', DateFormatLang: 'fr-FR'
-});
-$('#divRss6').FeedEk({
-FeedUrl: 'https://jquery-plugins.net/rss', MaxCount: 2, DateFormat: 'f', DateFormatLang: 'en'
-});
+  $("#divRss5").FeedEk({
+    FeedUrl: "https://jquery-plugins.net/rss",
+    MaxCount: 2,
+    DateFormat: "D",
+    DateFormatLang: "fr-FR",
+  });
+  $("#divRss6").FeedEk({
+    FeedUrl: "https://jquery-plugins.net/rss",
+    MaxCount: 2,
+    DateFormat: "f",
+    DateFormatLang: "en",
+  });
 
-$('#divRss10').FeedEk({
-FeedUrl: 'https://jquery-plugins.net/rss', MaxCount: 2, DateFormat: 'MM-dd-yyyy HH:mm', DateFormatLang: 'en'
-});
-$('#divRss11').FeedEk({
-FeedUrl: 'https://jquery-plugins.net/rss', MaxCount: 2, DateFormat: 'dd MMMM yyyy', DateFormatLang: 'en'
-});
-$('#divRss12').FeedEk({
-FeedUrl: 'https://jquery-plugins.net/rss', MaxCount: 2, DateFormat: 'MM/dd/yyyy'
-});
+  $("#divRss10").FeedEk({
+    FeedUrl: "https://jquery-plugins.net/rss",
+    MaxCount: 2,
+    DateFormat: "MM-dd-yyyy HH:mm",
+    DateFormatLang: "en",
+  });
+  $("#divRss11").FeedEk({
+    FeedUrl: "https://jquery-plugins.net/rss",
+    MaxCount: 2,
+    DateFormat: "dd MMMM yyyy",
+    DateFormatLang: "en",
+  });
+  $("#divRss12").FeedEk({
+    FeedUrl: "https://jquery-plugins.net/rss",
+    MaxCount: 2,
+    DateFormat: "MM/dd/yyyy",
+  });
 });
